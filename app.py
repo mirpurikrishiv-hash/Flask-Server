6ecbf6da648e449e32838114b1e3d022ddd477ea
mirpurikrishiv-hash <mirpurikrishiv@gmail.com>
2026-03-29 13:51:21 -0500

    pushing jokes server

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, request
+import requests
+
+app = Flask(__name__)
+
+# function to get a single non-programming joke
+def get_joke():
+    url = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single,twopart"
+    res = requests.get(url).json()
+
+    if res["type"] == "single":
+        return res["joke"]
+    else:
+        return f"{res['setup']} - {res['delivery']}"
+
+@app.route('/')
+def joke():
+    return f"<h2>{get_joke()}</h2>"
+
+@app.route("/many-jokes")
+def jokes():
+    # get count from URL, default = 5
+    count = int(request.args.get("count", 5))
+
+    jokes_list = [get_joke() for _ in range(count)]
+
+    return ''.join(f"<h2>{j}</h2>" for j in jokes_list)
+
+if __name__ == "__main__":
+    app.run(debug=True)
